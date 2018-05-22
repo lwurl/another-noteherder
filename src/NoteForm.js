68ed9bf5631612a964087b31b37bfc412746468c
Luke Wurl <lwurl@nd.edu>
2018-05-22 18:34:58 -0400

    Style NoteForm

--- src/NoteForm.js
+++ src/NoteForm.js
@@ -1,11 +1,63 @@
 import React from 'react';
+import './NoteForm.css';
 
 const NoteForm = () => {
     return (
-        <div className="NoteForm">
-            NoteForm
+        <div class="NoteForm" style={styles.noteForm}>
+          <div class="form-actions" style={styles.formActions}>
+            <button type="button" style={styles.button}>
+              <i class="fa fa-trash-o" style={styles.iButton}></i>
+            </button>
+          </div>
+          <form style={styles.form}>
+            <p>
+              <input
+                type="text"
+                name="title"
+                placeholder="Title your note"
+              />
+            </p>
+            
+            <textarea name="body" style={styles.textArea}></textarea>
+          </form>
         </div>
     );
+}
+
+const styles = {
+    noteForm: {
+        msFlexPositive: '1',
+        flexGrow: '1',
+        padding: '0 3rem',
+    },
+    formActions: {
+        paddingTop: '1rem',
+        marginLeft: '-2rem',
+        display: '-ms-flexbox',
+        display: 'flex',
+        msFlexLinePack: 'center',
+        alignContent: 'center',
+    },
+    button: {
+        border: 'none',
+        background: 'none',
+        padding: '0',
+    },
+    iButton: {
+        color: '#999',
+        fontSize: '2rem',
+        margin: '0',
+    },
+    form: {
+        margin: '0 auto',
+        maxWidth: '80rem',
+    },
+    textArea: {
+        borderColor: '#eee',
+        width: '100%',
+        height: 'calc(100vh - 140px)',
+        fontSize: '1.3em',
+    }
 }
 
 export default NoteForm;
