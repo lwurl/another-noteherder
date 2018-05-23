a9402c89ea2bffabc13c878265d4a871a110a591
Luke Wurl <lwurl@nd.edu>
2018-05-23 11:32:42 -0400

    Bring notes information up to Main

--- src/Main.js
+++ src/Main.js
@@ -4,17 +4,35 @@ import Sidebar from './Sidebar';
 import NoteList from './NoteList';
 import NoteForm from './NoteForm';
 
-const Main = () => {
-    return (
-        <div 
-            className="Main"
-            style={style}
-        >
-            <Sidebar />
-            <NoteList />
-            <NoteForm />
-        </div>
-    );
+class Main extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+            list: [
+                {
+                    title: 'Note 1',
+                    body: 'Body of the 1st note',
+                },
+                {
+                    title: 'Note 2',
+                    body: 'Body of the second note',
+                }
+            ]
+        }
+    }
+    
+    render() {
+        return (
+            <div 
+                className="Main"
+                style={style}
+            >
+                <Sidebar />
+                <NoteList listOfNotes={this.state.list}/>
+                <NoteForm />
+            </div>
+        );
+    }
 }
 
 const style = {
--- src/NoteList.js
+++ src/NoteList.js
@@ -3,28 +3,13 @@ import NoteListItem from './NoteListItem';
 import './NoteList.css';
 
 class NoteList extends React.Component {
-    constructor(){
-        super()
-        this.state = {
-            list: [
-                {
-                    title: 'Note 1',
-                    body: 'Body of the 1st note',
-                },
-                {
-                    title: 'Note 2',
-                    body: 'Body of the second note',
-                }
-            ]
-        }
-    }
 
     render() {
         return (
             <div class="NoteList">
             <h3>Notes</h3>
             <ul id="notes">
-                {this.state.list.map((info) => <NoteListItem title={info.title} body={info.body}/>)}
+                {this.props.listOfNotes.map((info) => <NoteListItem title={info.title} body={info.body}/>)}
             </ul>
             </div>
         );
